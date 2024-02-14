ff916ae6f98edada32d177bbd516c38ea00a9be6
Max Gabrielsson <max@gabrielsson.com>
2024-02-14 12:44:40 +0100

    try add headers + fp16lib

--- src/include/usearch/duckdb_usearch.hpp
+++ src/include/usearch/duckdb_usearch.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
+#include <unordered_map>
+#include <cfloat>
+
 #define USEARCH_USE_SIMSIMD 0
-#define USEARCH_USE_FP16LIB 0
+#define USEARCH_USE_FP16LIB 1
 #define USEARCH_USE_OPENMP  0
 
 #include "usearch/index.hpp"
